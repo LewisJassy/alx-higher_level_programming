03d6de33c5066769b40b4c57881a2c4e4ca9c20f
LewisJassy <jassylewis70@gmail.com>
2023-11-30 12:05:47 -0800

    number of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 
 if __name__ == "__main__":
     import sys
-    # Count the number of command-line arguments
+
     nargs = len(sys.argv) - 1
-    # Print information about the number of arguments
-    print("{:d} {:s}{:s}".format(nargs, "argument" if nargs <= 1 else "arguments",v"." if nargs == 0 else ":"))
-    # Iterate through command-line arguments and print them
+    print("{:d} {:s}{:s}".format(nargs,
+                                 "argument" if nargs <= 1 else "arguments",
+                                 "." if nargs == 0 else ":"))
     for i, arg in enumerate(sys.argv[1:]):
         print("{:d}: {:s}".format(i + 1, arg))

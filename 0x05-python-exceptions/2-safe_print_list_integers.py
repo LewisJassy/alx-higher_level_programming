962bdc118023971bcfea764b5e3e5cf145cd6068
LewisJassy <jassylewis70@gmail.com>
2023-12-18 21:18:30 +0300

    add an error message

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,5 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 if count == x:
                     break
         except IndexError:
-            break
+            print("IndexError: list out of range")
     return count

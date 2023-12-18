06c8b3bb97e59bf1266b8eba9d4c24c84f22455f
LewisJassy <jassylewis70@gmail.com>
2023-12-18 19:31:11 +0300

    removed the loop for it is not neccesary

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    while(value):
-        try:
-            if isinstance(value, int):
-                print("{:d}".format(value))
-                return True
-            else:
-                return False
-        except ValueError:
-            break
-    print()
-    return value
+    
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+    except ValueError:
+        return False

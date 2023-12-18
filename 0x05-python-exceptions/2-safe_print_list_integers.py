f16d2cfba41cdd9ef5f08b5dc59af20355577175
LewisJassy <jassylewis70@gmail.com>
2023-12-18 21:44:18 +0300

    removed if statement

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,10 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count =  0
     for i in range(0, x):
         try:
-            value =  my_list[i]
-            if isinstance(value, int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
         except (IndexError, TypeError):
             continue
     print()

85aacffa2d312f608939f3e72bf88cb88e901c74
LewisJassy <jassylewis70@gmail.com>
2023-12-18 19:22:27 +0300

    used while loop

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    for i in value:
+    while(value):
         try:
-            if isinstance(i, int):
-                print("{:d}".format(i))
+            if isinstance(value, int):
+                print("{:d}".format(value))
                 return True
             else:
                 return False

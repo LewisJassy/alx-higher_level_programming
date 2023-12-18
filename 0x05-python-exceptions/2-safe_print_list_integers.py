b65d653b5862a2ed92a9d5ce8ea797e71ec39911
LewisJassy <jassylewis70@gmail.com>
2023-12-18 21:41:24 +0300

    tabbed print()

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,5 +11,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         except (IndexError, TypeError):
             continue
-        print()
+    print()
     return count

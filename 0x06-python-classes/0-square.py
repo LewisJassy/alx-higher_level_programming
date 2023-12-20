6b99350e394f4767448b55d7857acf11801b07ca
LewisJassy <jassylewis70@gmail.com>
2023-12-21 02:53:12 +0300

    added an empty line

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Module for square operations"""
 
+
 class Square:
     """Represent a square.
 
-    This class is a simple placeholder for a square. It currently doesn't have any methods or attributes.
+    This class is a simple placeholder for a square.
+    It currently doesn't have any methods or attributes.
     """
     pass

215e9fe6eedc463e92f8c9e2edbb2388a2ce4ec3
LewisJassy <jassylewis70@gmail.com>
2023-12-21 02:15:34 +0300

    checked pycodestyle for bugs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,6 +4,7 @@
 class Square:
     """This is the Square class.
 
-    This class is a simple placeholder for a square. It currently doesn't have any methods or attributes.
+    This class is a simple placeholder for a square.
+    It currently doesn't have any methods or attributes.
     """
     pass

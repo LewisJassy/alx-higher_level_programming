73e2d387ec6f32491950bdda395a7551cbb2b408
LewisJassy <jassylewis70@gmail.com>
2024-01-03 20:52:50 +0300

    add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    This function adds two integers and returns their sum.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    return a + b

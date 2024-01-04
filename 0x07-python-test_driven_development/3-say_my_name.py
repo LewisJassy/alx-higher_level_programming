c1330433f67873fef563b9b7a58771bd85202259
LewisJassy <jassylewis70@gmail.com>
2024-01-04 15:23:57 +0300

    check if string empty

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,4 +5,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if last_name != str(last_name):
         raise TypeError("last_name must be a string")
+    if first_name == "" and last_name == "":
+        raise ValueError("String is empty")
     return first_name, last_name

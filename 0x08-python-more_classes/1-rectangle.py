46d055f84b501938679adb692f4f73ad0fea9f94
LewisJassy <jassylewis70@gmail.com>
2024-01-04 23:18:20 +0300

    remove blank line and whitespaces

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,12 +20,10 @@ class Rectangle:
 
         self.width = width
         self.height = height
-
     @property
     def width(self):
         """getter for width propety"""
         return self.__width
-
     @width.setter
     def width(self, value):
         """ setter for width property """
@@ -34,12 +32,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
     @property
     def height(self):
         """getter for height propety"""
         return self.__height
-
     @height.setter
     def height(self, value):
         """ setter for height property """

1782a3d3c18ebdf9059760a27999c6c01a045c07
LewisJassy <jassylewis70@gmail.com>
2024-01-04 22:16:33 +0300

    add empty line

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,9 +21,11 @@ class Rectangle:
         self.width = width
         self.height = height
 
+
     @property
     def width(self):
         return self.__width
+    
     
     @width.setter
     def width(self, value):
@@ -33,9 +35,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+
     @property
     def height(self):
         return self.__height
+    
     
     @height.setter
     def height(self, value):

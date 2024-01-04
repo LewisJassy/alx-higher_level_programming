c841d5a20448c4441653be736193bc0995e97873
LewisJassy <jassylewis70@gmail.com>
2024-01-04 15:32:13 +0300

    remove  if string empty

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -23,6 +23,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if first_name == "" and last_name == "":
-        raise ValueError("String is empty")
     return first_name, last_name

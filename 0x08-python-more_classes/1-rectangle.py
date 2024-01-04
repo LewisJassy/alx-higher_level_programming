6f56b6a4a09db8ff35e4c389a3411829b8084224
LewisJassy <jassylewis70@gmail.com>
2024-01-04 22:12:49 +0300

    added setters and properties

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,12 +15,17 @@ class Rectangle:
     TypeError: If width or height are not integers.
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.__width = 0
+        self.__height = 0
 
+        self.width = width
+        self.height = height
+
+    @property
     def width(self):
         return self.__width
     
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -28,9 +33,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         return self.__height
     
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")

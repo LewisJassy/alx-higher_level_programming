a39758fc9a8fb818f355d82dba3e55381136cc41
LewisJassy <jassylewis70@gmail.com>
2024-01-05 11:10:02 +0300

    change the for loop

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,16 +2,13 @@
 
 
 def print_square(size):
+    """Print a square of size size"""
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     if isinstance(size, float):
         raise ValueError("size must be an integer")
-    for i in range(size):
-        for j in range(size):
-            if i == 0 or i == size - 1 or j == 0 or j == size - 1:
-                print("#", end="")
-            else:
-                print(" ", end="")
-        print()
+    for _ in range(size):
+        print("#" * size)

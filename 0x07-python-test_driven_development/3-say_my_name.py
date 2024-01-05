30b4f2d0d489f8d048030851aa43f7b78ae981b2
LewisJassy <jassylewis70@gmail.com>
2024-01-05 10:44:38 +0300

    add a newline

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
 
-"""
-    This function takes in two strings as input and returns a tuple of two strings.
-    If the last_name parameter is not provided, it is initialized to an empty string.
-    If the first_name parameter is an empty string and the last_name parameter is also an empty string,
-    then the function raises a ValueError.
-    Otherwise, the function returns a tuple of the two strings.
 
-    Args:
-        first_name (str): The first name of the person.
-        last_name (str, optional): The last name of the person. Defaults to "".
-
-    Raises:
-        TypeError: If the input type is not a string.
-        ValueError: If the first_name parameter is an empty string and the last_name parameter is also an empty string.
-
-    Returns:
-        tuple: A tuple of two strings, which are the first and last names, respectively.
-    """
 def say_my_name(first_name, last_name=""):
+    """
+    prints a hello string for a name, handling input errors
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

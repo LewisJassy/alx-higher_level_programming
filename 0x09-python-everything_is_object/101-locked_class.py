c1e86fd0565be69d67f667e4f6100c4c7e35af62
LewisJassy <jassylewis70@gmail.com>
2024-01-06 03:31:35 +0300

    reurn attribute error

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,10 @@
 
 
 class LockedClass:
+    """Locked class: can't set instance attributes to it
+    """
     def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+        if name == "first_name":
+            self.__dict__[name] = value
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '" + name + "'")

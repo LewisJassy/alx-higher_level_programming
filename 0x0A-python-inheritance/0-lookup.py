5dba1155336475411ea04d5da252a96900adcbfb
LewisJassy <jassylewis70@gmail.com>
2024-01-09 22:46:59 +0300

    remove the statements

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    if isinstance(obj, dict):
-        return dict((lookup(k), lookup(v)) for k, v in obj.items())
-    elif isinstance(obj, list):
-        return [lookup(x) for x in obj]
-    else:
-        return obj
+    """Return list of attributes and methods of obj"""
+    return dir(obj)

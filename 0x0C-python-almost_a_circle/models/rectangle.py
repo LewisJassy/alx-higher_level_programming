20b10cc309c2af4d3ed5b3152ff0fd2a2a17ecad
LewisJassy <jassylewis70@gmail.com>
2024-01-13 01:03:26 +0300

    add a doc string

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,9 +80,5 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
     
     def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0] if len(args) > 0 else self.id
-            self.width = args[1] if len(args) > 1 else self.width
-            self.height = args[2] if len(args) > 2 else self.height
-            self.x = args[3] if len(args) > 3 else self.x
-            self.y = args[4] if len(args) > 4 else self.y
+        """Update attributes of the Rectangle instance with positional arguments."""
+        return f"[Rectangle] ({self.id}) {self.__width}/{self.__height} - {self.__x}/{self.__y}", args

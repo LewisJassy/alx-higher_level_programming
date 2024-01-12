0bc8b1d88b9d598d1cbf3063191fe817057f860b
LewisJassy <jassylewis70@gmail.com>
2024-01-12 23:35:01 +0300

    add a doc string

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,4 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + '#' * self.__width)
+        """Display the Rectangle instance by printing it to stdout using '#'."""

3d7ef377badc35cc9397f5376456d9b222a981ef
LewisJassy <jassylewis70@gmail.com>
2024-01-12 10:54:53 +0300

    create id attribute

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,7 @@
 class Base:
     __nb_objects = 0
     def __init__(self, id = None):
-        if self.id is not None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1

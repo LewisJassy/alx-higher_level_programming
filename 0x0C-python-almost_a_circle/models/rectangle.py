6b54bd694b19b83ea64f0e36b393b010db23169d
LewisJassy <jassylewis70@gmail.com>
2024-01-12 23:42:00 +0300

    string format

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + '#' * self.__width)
+
+    def __str__(self):
+        """function that returns values of rectangle in string format"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"

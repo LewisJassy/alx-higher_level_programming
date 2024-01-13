e541edc1f5ec7e5a0f7fedf3a3208a80fb42dba7
LewisJassy <jassylewis70@gmail.com>
2024-01-13 09:24:09 +0300

    remove validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,10 +15,7 @@ class Square(Rectangle):
     
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+        
         setattr(self, "width", value)
 
     def __str__(self):

2a9ed0290656d250a1f54ceb28e87241eda96070
LewisJassy <jassylewis70@gmail.com>
2024-03-16 09:05:24 +0300

    execute the file if exist

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+"""Script to list all states with name starting with `N` from
+database `hbtn_0e_0_usa`
+"""
 
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user = sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
-cur = db.cursor()
-cur.execute("SELECT *  FROM sttes WHERE name LIKE 'N%' ORDER BY id ASC")
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user = sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT *  FROM sttes WHERE name LIKE 'N%' ORDER BY id ASC")
 
-for row in cur.fetchall():
-    print(row)
+    for row in cur.fetchall():
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()

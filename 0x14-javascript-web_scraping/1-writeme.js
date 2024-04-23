d661e1ed7fdfb36d02204fa2ec225a0f8f22c37c
LewisJassy <jassylewis70@gmail.com>
2024-04-23 14:15:55 -0700

    semistandard fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 const filename = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filename, content, 'utf8', err => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(content);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(content);
 });
-

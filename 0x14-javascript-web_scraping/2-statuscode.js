3c44b71088e022209ffcc0b483fe162338e3cc2d
LewisJassy <jassylewis70@gmail.com>
2024-04-23 22:03:45 -0700

    update the code for coorerct statusCode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-
-const process = require("process");
+const process = require('process');
 const request = require('request');
-const server = process.argv[2];
 
-request(server, function (error, response, body) {
-	if (error != null) {
-		console.error("error:", error);
-	}
-	else {
-		console.log("code:", response.StatusCode);
-	}
+let url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error != null) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
 

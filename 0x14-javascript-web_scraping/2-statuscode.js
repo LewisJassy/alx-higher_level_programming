ba9094e495455b5c3e5c1638255e6e36d3419c35
LewisJassy <jassylewis70@gmail.com>
2024-04-23 22:05:00 -0700

    semistandard --fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const request = require('request');
 
-let url = process.argv[2];
+const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error != null) {
@@ -11,4 +11,3 @@ request(url, function (error, response, body) {
     console.log('code:', response.statusCode);
   }
 });
-

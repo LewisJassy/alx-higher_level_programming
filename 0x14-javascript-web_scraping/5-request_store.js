df4312bae03bc26c3d7a668df6466999f5dd953a
LewisJassy <jassylewis70@gmail.com>
2024-04-24 03:47:14 -0700

    wonder code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
-
 const request = require('request');
-const file = process.argv[3];
-const url = process.argv[2];
 const fs = require('fs');
+request.get(process.argv[2])
+  .on('error', err => console.log(err))
+  .pipe(fs.createWriteStream(process.argv[3], 'utf8'));
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  fs.writeFile(file, body, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(file);
-  });
-});
